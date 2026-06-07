033c7ab47978d12aec23cee9027614192f53e7b2
ShervanGharari <sh.gharari@gmail.com>
2026-06-06 22:18:26 -0600

    remove matplotlib from utility

--- src/riverlakenetwork/utility.py
+++ src/riverlakenetwork/utility.py
@@ -1,7 +1,6 @@
 import geopandas as gpd
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import subprocess
 import os
 from   shapely.geometry import Point, LineString, MultiLineString
