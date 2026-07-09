e9fc5c3662ca694f647c148e36b65cff2617e8bd
ShervanGharari <sh.gharari@gmail.com>
2026-07-09 14:00:52 -0600

    minor change in the test_

--- tests/test_1.py
+++ tests/test_1.py
@@ -213,13 +213,13 @@ def test1():
                     f"Mismatch in column '{col}' (showing first {len(idx)}):\n{details}"
                 )
 
-    TEST_DIR = Path(__file__).parent.parent / "test_1"
+    TEST_DIR = Path(__file__).parent.parent / "tests/test_1"
 
-    riv = gpd.read_file(TEST_DIR / "riv.shp")
+    riv = gpd.read_file(TEST_DIR / "riv.gpkg")
     assert_gdfs_equal(riv, bl.riv)
 
-    cat = gpd.read_file(TEST_DIR / "cat.shp")
+    cat = gpd.read_file(TEST_DIR / "cat.gpkg")
     assert_gdfs_equal(cat, bl.cat)
 
-    lake = gpd.read_file(TEST_DIR / "lake.shp")
+    lake = gpd.read_file(TEST_DIR / "lake.gpkg")
     assert_gdfs_equal(lake, bl.lake)
